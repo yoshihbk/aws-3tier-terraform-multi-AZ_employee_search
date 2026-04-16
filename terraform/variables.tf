49292054e4e46a30a1dc988f220b45d378e3bd8a
yoshihbk <yokoyamayoshihiro1995@outlook.jp>
2026-04-16 10:55:17 +0900

    Format variables.tf (terraform fmt auto-adjust)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,12 +9,12 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.0.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
