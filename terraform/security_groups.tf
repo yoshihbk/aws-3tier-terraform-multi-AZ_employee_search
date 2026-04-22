3a3e50548a389128e80393edb913750acc9e0d39
yoshihbk <yokoyamayoshihiro1995@outlook.jp>
2026-04-22 10:49:29 +0900

    Fix whitespace formatting in security_groups.tf

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]  # ALB SG のみ許可
+    security_groups = [aws_security_group.alb_sg.id] # ALB SG のみ許可
   }
 
   # -------------------------------------------------------
